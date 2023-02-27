02abf0ca32aa9ba7504c577e838063ddd3946d10
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-02-27 15:35:06 -0800

    Task 4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int len = strlen(s);
+for (int i = len - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
