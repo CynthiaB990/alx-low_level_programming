597bc172c7a25bd144397a91595e27a37f061bda
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-02-27 13:00:02 -0800

    Task 0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- *reset_to_98.c-resets the value of given integer to 98
- *@n: parameter
- *Return : nothing
+ * main - check the code 
+ *
+ * Return: Always 0.
  */
-int main(void) reset_to_98(int *n) {
-*n = 98;
+int main(void)
+{
+	void reset_to_98(int *n) {
+    *n = 98;
+}
 }
