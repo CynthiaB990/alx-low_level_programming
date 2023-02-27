898aa9134b1ac63d3ccdfaf54ad4c411792095f2
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-02-27 14:20:32 -0800

    Task 2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,18 +6,12 @@
  *Return: Always 0.
  */
 
-int _strlen(char *s) {
-    int len = 0;
-    while (*s != '\0') { // Keep incrementing len until the null terminator is reached
-        len++;
-        s++;
-    }
-    return len;
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0') { // Keep incrementing len until the null terminator is reached
+len++;
+s++;
 }
-
-int main() {
-    char str[] = "Hello, world!";
-    int len = _strlen(str);
-    printf("The length of the string is %d.\n", len);
-    return 0;
+return len;
 }
