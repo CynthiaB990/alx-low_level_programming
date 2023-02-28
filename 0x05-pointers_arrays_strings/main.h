57f93065d1e5ff8ddc450067cf5f3aeeae9201dc
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-02-27 16:27:11 -0800

    Task 4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Constants */
+#define MAX_STR_LEN 100
+
+/* Function prototypes */
+int sum_array(int *arr, int len);
+void reverse_array(int *arr, int len);
+int find_max(int *arr, int len);
+int string_length(char *str);
+void string_copy(char *dest, char *src);
+void string_reverse(char *str);
+void print_rev(char *s);
+
+#endif /* MAIN_H */
