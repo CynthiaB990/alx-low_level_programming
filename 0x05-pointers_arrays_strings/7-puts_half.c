e468bae553fc256bb340fe8370ceb902f6204ada
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-02-27 17:44:32 -0800

    Task 7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,12 @@
  */
 void puts_half(char *str)
 {
-    int len = strlen(str);
-    int i;
+int len = strlen(str);
+int i;
 
-    for (i = (len + 1) / 2; i < len; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+for (i = (len + 1) / 2; i < len; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
 }
