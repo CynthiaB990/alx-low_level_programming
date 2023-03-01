d84f796e65c360787b53837fbacd4423c0085d11
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-01 13:07:09 -0800

    Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ *cap_string - Capitalizes all words of a string
+ *@str: The string to be capitalized
+ *
+ *Return: A pointer to the capitalized string
+ */
+char *cap_string(char *str)
+{
+int i;
+
+
+i = 0;
+while (str[i])
+{
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"'
+|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+|| str[i - 1] == '}')
+str[i] = (str[i] >= 'a' && str[i] <= 'z') ? str[i] - 32 : str[i];
+i++;
+}
+return (str);
+}
