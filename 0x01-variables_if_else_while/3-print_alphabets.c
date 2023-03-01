36fb968ceacb832dd43e25d92e33ea102e42c842
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-02-28 17:00:03 -0800

    Task 3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char lowercase = 'a';
-    char uppercase = 'A';
-    
-    while (lowercase <= 'z')
-    {
-        putchar(lowercase);
-        lowercase++;
-    }
-    
-    while (uppercase <= 'Z')
-    {
-        putchar(uppercase);
-        uppercase++;
-    }
-    
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+
+    for (ch = 'A'; ch <= 'Z'; ch++)
+        putchar(ch);
+
     putchar('\n');
-    return 0;
+    return (0);
 }
