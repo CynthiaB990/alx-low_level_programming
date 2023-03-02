9f0406a9afa3ae885cc80ecd4d88fb6332037580
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-02 08:00:51 -0800

    Task 0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  *_strcat - appends the src string to the dest  
- *@dest - A pointer to the string tonate
- *@src - source of the string to be appended to @dest
+ *dest - A pointer to the string tonate
+ *src - source of the string to be appended to @dest
  *
  *Return: pointer to the destination of string @dest
  */
