64702683de1509c2cb759ccb53204882fc9372ef
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-04 16:52:22 -0800

    Task 4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
- *main - program that prints the alphabet in lowercase
+ *main-A  program that prints the alphabet in lowercase,followed by a new line.
+ *
  *Return: 0 (Success)
  */
 
