5818097f48b5d901fa563d7936e83205937170b6
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-06 10:10:12 -0800

    Task 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,19 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s)
+while (*s != '\0')
 {
 if (*s == c)
-return (s);
+{
+return s;
+}
 s++;
 }
 
 if (*s == c)
-return (s);
+{
+return s;
+}
 
-return (NULL);
+return NULL;
 }
