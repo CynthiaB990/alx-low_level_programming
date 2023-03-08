32579b14220443ece45bd6b6717722a3eff9b2c7
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-08 12:52:00 -0800

    Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string recursively followed by a new line
+ *@s: the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0') /* base case */
+{
+_putchar('\n')
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+
+return (0);
+}
