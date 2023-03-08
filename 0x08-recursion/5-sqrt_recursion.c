56265709138ed42e43f8e7fab65050be04c5157f
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-08 15:43:00 -0800

    Task 5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
 /**
- *_sqrt_recursion - returns the natural square root of a number
- *@n: the number to calculate the square root of
+ * _sqrt_recursion - computes the natural square root of a number
+ * @n: the number to compute the square root of
  *
- *Return: the natural square root of n, or -1 if n has no natural square root
+ * Return: the square root of n, or -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-if (n == 0 || n == 1)
-return (n);
-
-return (sqrt_helper(n, 1));
+if (n < 0) {
+return -1;
 }
-
-/**
- *sqrt_helper - calculates the square root of n recursively
- *@n: the number to calculate the square root of
- *@i: the current value to check for being the square root of n
- *
- *Return: the square root of n, or -1 if no square root exists
- */
-int sqrt_helper(int n, int i)
+else if (n == 0 || n == 1)
 {
+return n;
+}
+else
+{
+int i = 1;
+while (i * i < n)
+{
+i++;
+}
 if (i * i == n)
-return (i);
-if (i * i > n)
-return (-1);
-
-return (sqrt_helper(n, i + 1));
+{
+return i;
+}
+else
+{
+return -1;
+}
+}
 }
