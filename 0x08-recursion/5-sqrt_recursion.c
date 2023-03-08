6f63ecf307d4d47b738cc8c375e2e6582f29d30e
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-08 15:15:59 -0800

    Task 5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *@n: the number to calculate the square root of
+ *
+ *Return: the natural square root of n, or -1 if n has no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+
+return (sqrt_helper(n, 1));
+}
