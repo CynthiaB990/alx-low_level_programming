73960cd4d97ca5adad57535b6f28f1f6ab41565b
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-08 12:55:04 -0800

    Task 0.She locked away a secret , deepinside herself,something she once knew to be true

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>  
+#include <time.h>
 
 int main()
 {
-    int counter = 0;
-    srand(time(NULL));
-    char randchar;
+int counter = 0;
+srand(time(NULL));
+char randchar;
 
-    int  passwordLength;
+int  passwordLength;
 
-    printf("Type in a password Length \n");
-    scanf("%d", &passwordLength);
+printf("Type in a password Length \n");
+scanf("%d", &passwordLength);
 
-    while(counter < passwordLength)
-    {
-        //seed random based on time
-        srand(time(NULL));
-        randchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[random () % 62];    
-        printf("%c", randChar);
-        counter++;
-    }
-    return 0;
+while(counter < passwordLength)
+{
+//seed random based on time
+srand(time(NULL));
+randchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[random () % 62];
+printf("%c", randChar);
+counter++;
+}
+return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 0x02-C functions nested loops
 0X05-C pointers, arrats, strings
 0x02. C - Functions, nested loops
+0x06. C - More pointers, arrays and strings
+0x07. C - Even more pointers, arrays and strings
+0x08-recursion
