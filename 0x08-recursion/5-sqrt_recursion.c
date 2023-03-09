0ed55dfd524d0ddb8c2fd14ce423045e4e549418
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-08 16:02:49 -0800

    Task 7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,11 @@
 #include <stdio.h>
 
 /**
- *_sqrt_recursion - returns the natural square root of a number
- *@n: the number to calculate the square root of
+ *find_sqrt - finds the natural square root of the number
+ *@num: the number to find the square root of
+ *@root: the root to be tested.
  *
- *Return: the natural square root of n, -1 if n does not have a natural square root
+ *Return: the natural square root -square root, -1 if if it does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return -1;
-
-if (n == 0 || n == 1)
-return n;
-
-return _sqrt(n, 1);
-}
-
-/**
- *_sqrt - returns the natural square root of a number using recursion
- *@n: the number to calculate the square root of
- *@i: the current guess
- *
- *Return: the natural square root of n, -1 if n does not have a natural square root
- */
-int _sqrt(int n, int i)
-{
-if (i * i == n)
-return i;
-
-if (i * i > n)
-return -1;
-
-return _sqrt(n, i + 1);
-}
