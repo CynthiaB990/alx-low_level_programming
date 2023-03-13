16e936b92806d16232de10351faedd89cf740ee0
Cynthia Barasa <cynthiabarasa312@gmail.com>
2023-03-13 06:17:27 -0700

    Task 0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ *main - prints the name of the programs
  *@argc: The number of command-line arguments
  *@argv: An array of strings containing the arguments
  *
@@ -9,6 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%s\n", *argv[0]);
+
 return (0);
 }
